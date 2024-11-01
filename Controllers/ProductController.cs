f6e2df006b50cc398cc0a9da78a7458c933aa9a5
TheNeyzi <166630157+TheNeyzi4@users.noreply.github.com>
2024-11-01 21:58:58 +0200

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using shop_app.Models;
+using shop_app.Services;
+
+namespace shop_app.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IServiceProduct _serviceProduct;
+        public ProductController(IServiceProduct serviceProduct)
+        {
+            _serviceProduct = serviceProduct;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Read()
+        {
+            var products = await _serviceProduct.ReadAsync();
+            return View(products);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _serviceProduct.GetByIdAsync(id);
+            return View(product);
+        }
+        [HttpGet]
+        public IActionResult NotFound() => View();
+        [HttpGet]
+        public IActionResult Create() => (User.IsInRole("admin")) ? View() : RedirectToAction("NotFound");
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price, Description")] Product product)
+        {
+            if (ModelState.IsValid && (User.IsInRole("admin") || User.IsInRole("moderator")))
+            {
+                _ = await _serviceProduct.CreateAsync(product);
+                return RedirectToAction(nameof(Read));
+            }
+            return View(product);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Update(int id) 
+        {
+            var product = await _serviceProduct.GetByIdAsync(id);
+            return View(product);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int id, [Bind("Id,Name,Price, Description")] Product product)
+        {
+            if (ModelState.IsValid && (User.IsInRole("admin") || User.IsInRole("moderator")))
+            {
+                _ = await _serviceProduct.UpdateAsync(id, product);
+                return RedirectToAction(nameof(Read));
+            }
+            return View(product);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var productId = await _serviceProduct.GetByIdAsync(id);
+            if (productId is null)
+            {
+                return NotFound();
+            }
+            return View(productId);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (User.IsInRole("admin"))
+            {
+                _ = await _serviceProduct.DeleteAsync(id);
+                return RedirectToAction(nameof(Read));
+            }
+            return RedirectToAction(nameof(Read));
+        }
+
+    }
+}
---
+++ Services/ServiceProduct.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shop_app.Models;
+
+namespace shop_app.Services
+{
+    public interface IServiceProduct
+    {
+        Task<Product> CreateAsync(Product? Product);
+        Task<IEnumerable<Product>> ReadAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task<Product> UpdateAsync(int id, Product? product);
+        Task<bool> DeleteAsync(int id);
+    }
+    public class ServiceProduct : IServiceProduct
+    {
+        private readonly ProductContext _productContext;
+        private readonly ILogger<ServiceProduct> _logger;
+        public ServiceProduct(ProductContext productContext, ILogger<ServiceProduct> logger)
+        {
+            _productContext = productContext;
+            _logger = logger;
+        }
+
+        public async Task<Product> CreateAsync(Product? product)
+        {
+            if (product is null)
+            {
+                _logger.LogWarning("Attempt create a product with null");
+                return null;
+            }
+            await _productContext.AddAsync(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var product = await _productContext.Products.FindAsync(id);
+
+            if (product is null)
+            {
+                return false;
+            }
+            _productContext.Remove(product);
+            await _productContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _productContext.Products.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Product>> ReadAsync()
+        {
+            return await _productContext.Products.ToListAsync();
+        }
+
+        public async Task<Product> UpdateAsync(int id, Product? product)
+        {
+            if (product is null || id != product.Id)
+            {
+                _logger.LogWarning($"{nameof(Product)}: {id}");
+            }
+            try
+            {
+                _productContext.Products.Update(product);
+                await _productContext.SaveChangesAsync();
+                return product;
+            } catch (DbUpdateConcurrencyException ex)
+            {
+                _logger.LogError(ex.Message);
+                return null;
+            }
+        }
+    }
+}
